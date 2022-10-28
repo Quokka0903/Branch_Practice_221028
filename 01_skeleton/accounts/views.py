b8e81a1e6d563017b25c875d8bd87f4d87747dbc
이정규 <kue411@naver.com>
2022-10-28 15:51:20 +0900

    follow JS로 구현

--- 01_skeleton/accounts/views.py
+++ 01_skeleton/accounts/views.py
@@ -118,6 +118,11 @@ def follow(request, user_pk):
         me = request.user
         you = get_object_or_404(get_user_model(), pk=user_pk)
 
+        #print('me')
+        #print(me)
+        #print('you')
+        #print(you)
+
         if me != you:
             if you.followers.filter(pk=me.pk).exists():
                 you.followers.remove(me)
@@ -126,8 +131,11 @@ def follow(request, user_pk):
                 you.followers.add(me)
                 is_followed = True
 
+            follow_count = you.followers.count()
+
             context = {
                 'is_followed': is_followed,
+                'follow_count' : follow_count
             }
             return JsonResponse(context)
         #return redirect('accounts:profile', you.username)
